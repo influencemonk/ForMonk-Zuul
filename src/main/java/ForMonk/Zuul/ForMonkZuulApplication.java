d1e1e4f8eb04eac7219d8b7459faa27c9babb2a9
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-05-15 18:48:08 +0530

    First commit

---
+++ src/main/java/ForMonk/Zuul/ForMonkZuulApplication.java
@@ -0,0 +1,63 @@
+package ForMonk.Zuul;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import ForMonk.Zuul.Filters.ErrorFilter;
+import ForMonk.Zuul.Filters.PostFilter;
+import ForMonk.Zuul.Filters.PreFilter;
+import ForMonk.Zuul.Filters.RouteFilter;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ForMonkZuulApplication implements SwaggerResourcesProvider {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMonkZuulApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+	    return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+	    return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+	    return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+	    return new RouteFilter();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<SwaggerResource> get() {
+		// TODO Auto-generated method stub
+		 	List resources = new ArrayList<>();
+	        resources.add(swaggerResource("post-scheduler", "/docApi/v2/api-docs", "2.0"));
+	        return resources;
+	}
+	
+	private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+	
+}
+	
